f1e25be142235c054c6190dc6af439f53786a7e7
Dsky7 <sakai7213@yahoo.co.jp>
2015-06-01 11:42:52 +0900

    BatteryMonitor_ina226.ino

--- SimpleSerial/SimpleSerial.ino
+++ SimpleSerial/SimpleSerial.ino
@@ -1,12 +1,19 @@
+int i1;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
+  for(i1=0;i1<255;i1++){  Serial.print(i1,DEC);Serial.write(',');Serial.println(255-i1,DEC);  delay(100);}
+
+
+
   // put your main code here, to run repeatedly:
 while (Serial.available() > 0) {
-  Serial.write( Serial.read()+1);
+  //Serial.write( Serial.read()+1);
+
   Serial.println("");
+  delay(100);
 }
 }
