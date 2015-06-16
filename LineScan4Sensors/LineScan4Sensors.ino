c678caf01a36035755688ab6b7753d4bc4a99867
Dsky7 <sakai7213@yahoo.co.jp>
2015-06-16 11:58:15 +0900

    kannsei mae

--- LineScan4Sensors/LineScan4Sensors.ino
+++ LineScan4Sensors/LineScan4Sensors.ino
@@ -1,37 +1,71 @@
-#include <Adafruit_NeoPixel.h>
-#include <avr/power.h>
-#define DelayClock  20
-#define PIN            6
-#define NUMPIXELS      8
+#include <EEPROM.h>
+#include <Wire.h>
+
 #define DEBUG
-#define  maskFR    20
-#define  maskRL    40
+#define DelayClock  1
+#define  AddWhite  128
+#define  AddBlack  512
+int        maskFR = 40, maskRL = 50;
 const int analogInPin1 = A0, analogInPin2 = A1, analogInPin3 = A8, analogInPin4 = A10;
 const int CLK = 4;
 const int SI = 11;
-int  threshold=100,thresholdLOW,thresholdHIGH;
+const int CaliLED = 0;
+const int CaliButton = 1;
+int    time1, time2;
+long    LoopTime, oldLoopTime;
+int state = 0;
+int  value[128];
+int    adata;
+int  threshold = 100, thresholdLOW, thresholdHIGH;
 byte Adata;
+byte sendData[8], TransmitData[4];
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 int AnalogValue1[128], AnalogValue2[128], AnalogValue3[128], AnalogValue4[128];
-int i1;
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int Data[32];
+int i1, i2, i3;
+int LineQuantity = 0;
+long aveLinePos;
+void sendPC(int *temp);
+void maskData(int *temp, int maskTH);
+void sendPC_Debug(int *temp);
+void sendPC_Byte(int *temp);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   pinMode(CLK, OUTPUT);
   pinMode(SI, OUTPUT);
+  pinMode(CaliLED, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(CaliButton, INPUT_PULLUP);
+  attachInterrupt(3, interruptsw, LOW ); // (1)
   digitalWrite(CLK, LOW);
+  Wire.begin(4);
+  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent); // register event
+  ADCSRA = ADCSRA & 0xf8;
+  ADCSRA = ADCSRA | 0x04;
+}
 
-  pixels.begin();
-  for (int i = 0; i < NUMPIXELS; i++) {
-    if (i < (NUMPIXELS / 2)) {
-      pixels.setPixelColor(i, pixels.Color(200 * (4 - i) / 4, 200, 200));
-    } else {
-      pixels.setPixelColor(i, pixels.Color(200 - (NUMPIXELS - 1 - i) * 25, 0, 200));
-    }
+void GetDataOne(int *temp , int pinNum) {
+  digitalWrite(SI, HIGH);
+  delayMicroseconds(DelayClock);
+
+  for (i1 = 0; i1 < 128; i1++) {
+    digitalWrite(CLK, HIGH);
+    delayMicroseconds(DelayClock);
+
+    digitalWrite(SI, LOW);
+
+    temp[i1] = analogRead(pinNum);
+    digitalWrite(CLK, LOW);
+    delayMicroseconds(DelayClock);
+
   }
-  pixels.show(); // This sends the updated pixel color to the hardware.
+  digitalWrite(CLK, HIGH);
+  delayMicroseconds(DelayClock);
+  digitalWrite(CLK, LOW);
+  delayMicroseconds(20);
 }
 
 void GetData(void) {
@@ -55,44 +89,169 @@ void GetData(void) {
   digitalWrite(CLK, HIGH);
   delayMicroseconds(DelayClock);
   digitalWrite(CLK, LOW);
-  delayMicroseconds(300);
+  //  delayMicroseconds(300);
 }
 
+void CalibrationBlack(void) {
+  for (i1 = 0; i1 < 128; i1++) {
+    EEPROM.write(AddBlack + i1,  AnalogValue1[i1] / 4); //AnalogValue1[i1]
+  }
+  for (i1 = 0; i1 < 128; i1++) {
+    value[i1] = EEPROM.read(AddBlack + i1) * 4;
+  }
+
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+}
+
+int CalculateLine(int *AValue, int mask) {
+  LineQuantity = 0; aveLinePos = 0;
+  for (i1 = mask; i1 < 128 - mask; i1++) {
+    if (AValue[i1] / 4 < threshold) {
+      LineQuantity ++;
+      aveLinePos = aveLinePos + i1;
+    }
+  }
+  if (LineQuantity == 0) {
+    return 21;
+  }
+  return (int)aveLinePos / LineQuantity;
+}
+void CalibrationWhite(void) {
+  //GetData();
+
+  for (i1 = 0; i1 < 128; i1++) {
+    EEPROM.write(AddWhite + i1, AnalogValue1[i1] / 4) ;
+  }
+
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+}
+
+
 void loop() {
-  GetData();
-  Serial.print("st");
+  LoopTime = millis() - oldLoopTime;
+  oldLoopTime = millis();
+  GetDataOne(AnalogValue1, analogInPin1);
+  time1 = millis();
 
 #if defined (DEBUG)
-  for (i1 = 0; i1 < 128; i1++) {
-    Adata = AnalogValue1[i1] / 4;
-    Serial.write(Adata);
+  Serial.print("st");
+  sendPC_Debug(AnalogValue1);
+#endif
+  GetDataOne(AnalogValue2, analogInPin2);
+#if defined (DEBUG)
+  sendPC_Debug(AnalogValue2);
+#endif
+  GetDataOne(AnalogValue3, analogInPin3);
+#if defined (DEBUG)
+  sendPC_Debug(AnalogValue3);
+#endif
+  GetDataOne(AnalogValue4, analogInPin4);
+#if defined (DEBUG)
+  sendPC_Debug(AnalogValue4);
+#endif
+  time2 = millis();
+  Data[0] = LoopTime;
+  Data[1] = threshold;
+  Data[2] = maskFR;
+  Data[3] = maskRL;
+
+  Data[4] = CalculateLine(AnalogValue1, maskFR);
+  Data[5] = CalculateLine(AnalogValue2, maskFR);
+  Data[6] = CalculateLine(AnalogValue3, maskRL);
+  Data[7] = CalculateLine(AnalogValue4, maskRL);
+  TransmitData[0] = Data[4];
+  TransmitData[1] = Data[5];
+  TransmitData[2] = Data[6];
+  TransmitData[3] = Data[7];
+#if defined DEBUG
+  for (i1 = 0; i1 < 4; i1++) {
+    sendData[i1] = Data[i1 + 4];
   }
-  for (i1 = 0; i1 < 128; i1++) {
-    Adata = AnalogValue2[i1] / 4;
-    Serial.write(Adata);
-  }
-    for (i1 = 0; i1 < 128; i1++) {
-    Adata = AnalogValue3[i1] / 4;
-    Serial.write(Adata);
-  }
-  for (i1 = 0; i1 < 128; i1++) {
-    Adata = AnalogValue4[i1] / 4;
-//   Adata=150;
-    Serial.write(Adata);
-  }
-  #else
-  for (i1 = 0; i1 < 128; i1++) {
-    Adata = AnalogValue1[i1] / 4;
-    if(Adata<threshold){Adata=0;}else{Adata=threshold;}
-    Serial.write(Adata);
-  }
-  for (i1 = 0; i1 < 128; i1++) {
-    Adata = AnalogValue2[i1] / 4;
-        if(Adata<threshold){Adata=0;}else{Adata=threshold;}
-    Serial.write(Adata);
-  }  
-  
+    sendPC_Byte(Data);
 #endif
 
+  if (state == 1) {
+    delay(100);
 
+    CalibrationBlack();
+
+    digitalWrite(CaliLED, 1);
+    delay(1000);
+    digitalWrite(CaliLED, 0);
+    state = 2;
+    attachInterrupt(3, interruptsw, LOW );
+  } else if (state == 3) {
+
+    delay(100);
+    CalibrationWhite();
+
+    digitalWrite(CaliLED, 1);
+    delay(100);
+    digitalWrite(CaliLED, 0);
+    delay(100);
+    digitalWrite(CaliLED, 1);
+    delay(100);
+    digitalWrite(CaliLED, 0);
+    delay(100);
+
+    state = 0;
+    attachInterrupt(3, interruptsw, LOW );
+  }
+
+}
+void maskData(int *temp, int maskTH) {
+  for (i1 = 0; i1 < maskTH; i1++) {
+    temp[i1] = 1023;
+  }
+  for (i1 = 128 - maskTH; i1 < 128; i1++) {
+    temp[i1] = 1023;
+  }
+}
+void sendPC_Debug(int *temp) {
+
+  for (i1 = 0; i1 < 128; i1++) {
+    Adata = temp[i1] / 4;
+    Serial.write(Adata);
+  }
+}
+void sendPC_Byte(int *temp) {
+  for (i1 = 0; i1 < 32; i1++) {
+    Adata = temp[i1];
+    Serial.write(Adata);
+  }
+}
+void sendPC(int *temp) {
+
+  for (i1 = 0; i1 < 128; i1++) {
+    Adata = temp[i1] / 4;
+    if (Adata < threshold) {
+      Adata = 0;
+    } else {
+      Adata = threshold;
+    }
+    Serial.write(Adata);
+  }
+}
+void interruptsw()
+{
+  detachInterrupt(3);
+  state  ++;
+}
+void requestEvent()
+{
+  Wire.write(TransmitData, 4);
+}
+void receiveEvent(int howMany)
+{
+  i3 = 0;
+
+  while (0 < Wire.available()) // loop through all but the last
+  {
+    Data[8 + i3] = Wire.read(); // receive byte as a character
+    i3++;
+  }
 }
---
+++ SFT_SUB_1.0v/SFT_SUB_1.0v.ino
@@ -0,0 +1,154 @@
+#include <RCS620S.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <inttypes.h>
+#include <string.h>
+
+//#define    debug
+#define    AddSub1    11
+#define COMMAND_TIMEOUT  400
+#define PUSH_TIMEOUT    2100
+#define POLLING_INTERVAL 500
+#define NUMPIXELS      24
+#define LED_PIN 13
+#define PIN            10
+RCS620S rcs620s;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int waitCardReleased = 0;
+int   i1, i2, i3;
+int   id[10][8];
+int   maxnumRFID, numRFID, IDaccord;
+byte  ReceiveData[8];
+int ret;
+byte  TransmitData[8];
+
+void setup()
+{
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  pixels.begin();
+  /*Wire*/
+  Wire.begin(AddSub1);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent); // register event
+  /*other*/
+  digitalWrite(LED_PIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  /*Serial*/
+  Serial1.begin(115200);
+#if defined(debug)
+  Serial.begin(9600);
+#endif
+  ret = rcs620s.initDevice();
+  while (!ret) {}
+  digitalWrite(LED_PIN, LOW);
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(200, 50, 50)); // Moderately bright green color.
+    delay(10);
+  } pixels.show(); // This sends the updated pixel color to the hardware.
+}
+
+void loop()
+{
+  int ret;
+  IDaccord = 0;
+  digitalWrite(LED_PIN, HIGH);
+  // rcs620s.timeout = COMMAND_TIMEOUT;
+  ret = rcs620s.polling();
+  for (i1 = 0; i1 < 8; i1++) {
+    id[0][i1] = rcs620s.idm[i1];
+  }
+  for (i3 = 1; i3 < 10; i3++) {
+    for (i1 = 0; i1 < 8; i1++) {
+      if (id[i3][i1] != id[0][i1]) {
+        break;
+      }
+      if (i1 == 7) {
+        IDaccord = 1;
+        numRFID = i3;
+      }
+    }
+    if (IDaccord == 1) {
+      break;
+    }
+    if (i3 == 9) {
+      numRFID = maxnumRFID + 1;
+      maxnumRFID++;
+    }
+  }
+  for (i2 = 0; i2 < 8; i2++) {
+
+#if defined (debug)
+    Serial.print(rcs620s.idm[i2]);
+    Serial.print(" ");
+    #endif
+  }
+  #if defined(debug)
+  Serial.println("");
+#endif
+  TransmitData[0] = numRFID;
+  TransmitData[1] = rcs620s.idm[2];
+  TransmitData[2] = rcs620s.idm[3];
+#if defined(debug)
+  Serial.print(numRFID);
+#endif
+  for (i1 = 0; i1 < 8; i1++) {
+    id[numRFID][i1] = id[0][i1];
+#if defined(debug)
+    Serial.print(id[numRFID][i1]);
+    Serial.print(" ");
+#endif
+  }
+#if defined(debug)
+  Serial.println("");
+#endif
+  //  if (!ret) {
+  //    if (waitCardReleased) {
+  //      waitCardReleased = 0;
+  //    }
+  //  } else if (!waitCardReleased) {
+  //    // Push
+  //    uint8_t data[] = {
+  //      0x01, 0x02, 0x2a, 0x00, 0x1d, 0x00, 0x68, 0x74,
+  //      0x74, 0x70, 0x3a, 0x2f, 0x2f, 0x74, 0x77, 0x69,
+  //      0x74, 0x74, 0x65, 0x72, 0x2e, 0x63, 0x6f, 0x6d,
+  //      0x2f, 0x53, 0x44, 0x4b, 0x34, 0x46, 0x65, 0x6c,
+  //      0x69, 0x43, 0x61, 0x46, 0x6f, 0x6c, 0x6c, 0x6f,
+  //      0x77, 0x20, 0x4d, 0x65, 0x21, 0x21, 0xf1, 0xee,
+  //    };
+  //    rcs620s.timeout = PUSH_TIMEOUT;
+  //    ret = rcs620s.push(data, sizeof(data));
+  //    if (ret) {
+  //      waitCardReleased = 1;
+  //    }
+  //  }
+
+  // rcs620s.rfOff();
+  digitalWrite(LED_PIN, LOW);
+  // delay(POLLING_INTERVAL);
+}
+void receiveEvent(int howMany)
+{
+  while (1 < Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read(); // receive byte as a character
+    //Serial.print(c);         // print the character
+  }
+  ReceiveData[0] = Wire.read();    // receive byte as an integer
+#if defined(debug)
+  Serial.print("ReceiveData[0] = ");
+  Serial.println(ReceiveData[0]);         // print the integer
+#endif
+}
+void requestEvent()
+{
+  Wire.write(TransmitData, 4);
+  //#if defined (debug)
+  //  Serial.print("TransmitData[0] = ");
+  //  Serial.print(TransmitData[0]); Serial.print(" "); Serial.print(TransmitData[1]); Serial.print(" "); Serial.println(TransmitData[2]);     // print the integer
+  //#endif
+}
